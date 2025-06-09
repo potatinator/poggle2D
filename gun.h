03090fa49000852b87f56edc0bf159ea360c67e8
Julien Bloom <alpentaljulien@gmail.com>
2025-06-09 10:43:32 -0700

    fuxed phys

--- gun.h
+++ gun.h
@@ -9,6 +9,7 @@ class Gun : public TexturedGameObject{
     public:
         glm::vec2 mousePos;
         Ball* ball;
+        int count = 0;
         Gun(glm::vec2 pos, Texture2D tex, Ball* ball, glm::vec2 scale=glm::vec2(1.0f, 1.0f), int layer=0, float angle=0.0f);
         void update(float dt);
         void draw();
