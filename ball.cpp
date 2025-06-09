ae5d6ccebdf41291908d76a0ffff41d956f90a26
Julien Bloom <alpentaljulien@gmail.com>
2025-06-09 09:16:46 -0700

    egg

--- ball.cpp
+++ ball.cpp
@@ -5,7 +5,8 @@
 Ball::Ball(glm::vec2 pos, glm::vec2 vel, std::vector<Peg*>* pegs) : pegs(pegs), velocity(vel), active(false), TexturedGameObject(pos, Texture2D("./resources/ball1.png", true), glm::vec2(15.0f)) {};
 void Ball::update(float dt){
     if(active){
-        velocity += glm::vec2(0.0, 300.0)*dt;
+        velocity += glm::vec2(0.0, 400.0)*dt;
+        velocity -= glm::vec2(0.2, 0.2)*dt;
         position += velocity*dt;
         if(position.y > 600){
             active = false;
@@ -24,8 +25,8 @@ void Ball::collide(){
             if(pegs->at(i) != nullptr){
                 glm::vec2 diff = position - pegs->at(i)->position;
                 if(glm::length(diff) <= scale.x/2.0+pegs->at(i)->scale.x/2.0){
-                    position += glm::vec2((20.0-glm::length(diff))) * glm::normalize(diff);//position resolution
-                    velocity = velocity - glm::vec2(2.0) * glm::dot(velocity, glm::normalize(diff)) * glm::normalize(diff);
+                    position += glm::vec2((20.0-glm::length(diff))) * glm::normalize(diff); //position resolution
+                    velocity = velocity - glm::vec2(1.94) * glm::dot(velocity, glm::normalize(diff)) * glm::normalize(diff);
                     hits.push_back(i);
                 }  
             } 
