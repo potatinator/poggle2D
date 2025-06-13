7308cf775f673b82dc0d1571f3b00a45f3dc6143
kennamak22 <kennamak22@gmail.com>
2025-06-13 09:45:33 -0700

    bucket

--- bucket.cpp
+++ bucket.cpp
@@ -3,7 +3,7 @@
 #include "bucket.h"
 #include <iostream>
 
-Bucket::Bucket() :  velocity(glm::vec2(300, 0)), TexturedGameObject(glm::vec2(0, 575), Texture2D("./resources/placeholder.png", true), glm::vec2(150, 50), 1) {};
+Bucket::Bucket() :  velocity(glm::vec2(300, 0)), TexturedGameObject(glm::vec2(0, 575), Texture2D("./resources/bucket.png", true), glm::vec2(150, 50), 1) {};
 void Bucket::update(float dt){
     position += velocity*dt;
     if(position.x < 0){
